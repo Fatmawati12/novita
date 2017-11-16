7665fce8655480499e8b206f9ee3cd4c43d3d938
Aji Kamaludin <aji19kamaludin@gmail.com>
2017-11-16 15:50:00 +0700

    baru sampai di tampil data, mau membuat input

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+	$host = "localhost";
+	$user = "root";
+	$password = "eta";
+	$dbname = "AppInventori";
+
+	$koneksi = mysqli_connect($host,$user,$password,$dbname);
+
+	/*if($koneksi){
+		echo "sukses";
+	}else{
+		echo "koneksi gagal";
+	}*/
